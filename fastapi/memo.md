6ae8c1a3a06d7a0460e56496160886d3d3c031df
koga.shinya <shinya4study@gmail.com>
2025-06-22 15:02:01 +0900

    fastapi succession

--- fastapi/memo.md
+++ fastapi/memo.md
@@ -10,3 +10,65 @@
 
 `http://127.0.0.1:8000/openapi.json`これでJSONの情報を確認できる
 
+
+`def read_items(q: list[str] | None = Query(default=None, min_length=2, max_length=50)):`
+ここでqのバリデーションを定義しているが、Listにすると要素が2個以上になり、strにすると2文字以上になる
+
+`http://localhost:8000/items/?q=foo&q=bar`で情報を取得
+
+一般的なバリデーションとメタデータ:
+
+- alias
+- title
+- description
+- deprecated
+
+文字列のためのバリデーション:
+
+- min_length
+- max_length
+- regex
+
+
+また、数値のバリデーションを宣言することもできます:
+
+- gt: より大きい（greater than）
+- ge: 以上（greater than or equal）
+- lt: より小さい（less than）
+- le: 以下（less than or equal）
+
+exampleとして、具体例を設定できる
+```py
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                }
+            ]
+        }
+    }
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
+```
--- fastapi/tutorial.py
+++ fastapi/tutorial.py
@@ -1,17 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path, Body    
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, HttpUrl
 
 class ModelName(str, Enum):
     resnet = "resnet"
     t5 = "t5"
     visiontransformer = "visiontransformer"
+    
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+    description: str | None = Field(
+        # 追加引数として、exampleを設定できる
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
+    tax: float | None = None    # dict[int, float]も使用可能
+    tags: set[str] = set()
+    image: list[Image] | None = None
+    
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
 
 app = FastAPI()
 
@@ -28,5 +41,49 @@ async def get_model(model_name: ModelName):
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
+@app.put("/items/{item_id}")
+async def update_item(
+    *,
+    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+    item: Item,
+    user: User,
+    importance: int = Body(embed=True),    # embed=Trueで単一のボディパラメータを埋め込める
+    q: str | None = None):
+    result = {"item_id": item_id, **item.dict(), "user": user}
+    if q:
+        result.update({"q": q})
+    if item:
+        result.update({"item": item})
+    return result
+
+@app.get("/items/")
+async def read_items(
+    q: list[str] | None = Query(
+        default=None,
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=2, max_length=50),
+        # deprecated=True を指定すると非推奨パラメータであることを明示できる。らしい？？
+    ):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+# pattern="^fixedquary$"で正規表現を利用可能
+
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: int = Path(title="The ID of the item to get", gt=0, le=1000),
+    q: str | None = Query(default=None, alias="item-quary"),
+    size: float = Query(gt=0, lt=10.5),
+    ):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
