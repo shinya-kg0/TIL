04f94f994339ea0aa091e38f8bab0759a81976d3
koga.shinya <shinya4study@gmail.com>
2025-07-06 21:30:12 +0900

    react useState

--- react/finance-app/src/App.tsx
+++ react/finance-app/src/App.tsx
@@ -1,9 +1,28 @@
 import { Box, Card, CardBody, CardHeader, Button, ChakraProvider, Checkbox, Flex, Input, Text } from '@chakra-ui/react'
 import './App.css'
+import { useState } from 'react'
 
+type Record = {
+  id: number
+  title: string
+  isIncome: boolean
+  amount: number
+}
 
 function App() {
+  const [records, setRecords] = useState<Record[]>([])
+  const [title, setTitle] = useState<string>("")
+  const [isIncome, setIsIncome] = useState<boolean>(false)
+  const [amount, setAmount] = useState<number>(0)
 
+  const handleRecords = () => {
+    setRecords([...records, 
+      {id: records.length + 1, "title": title, "isIncome": isIncome, "amount": amount}])
+    setTitle("")
+    setAmount(0)
+    setIsIncome(false)
+    }
+  
 
   return (
     <ChakraProvider>
@@ -14,11 +33,23 @@ function App() {
           </CardHeader>
           <CardBody>
             <div>
-              <Input placeholder='支出を入力' mb="4px" />
+              <Input placeholder='タイトルを入力'mb="4px" value={title} onChange={(e) => setTitle(e.target.value)} />
+              <Input placeholder='支出を入力' mb="4px" value={amount} onChange={(e) => setAmount(Number(e.target.value))} />
               <Flex align="center" justifyContent="space-between">
-                <Checkbox w="100px">入金</Checkbox>
-                <Button colorScheme='teal'>追加</Button>
+                <Checkbox w="100px" isChecked={isIncome} onChange={() => setIsIncome(!isIncome)}>入金</Checkbox>
+                <Button colorScheme='teal' onClick={handleRecords}>追加</Button>
               </Flex>
+            </div>
+            <div>
+              {records.map((data) => (
+                <div key={data.id}>
+                  <Flex align="center" justifyContent="space-between">
+                    <Text>{data.title}</Text>
+                    <Text>{data.isIncome ? "+" : "-"}{data.amount}</Text>
+                  </Flex>
+                </div>
+              ))}
+
             </div>
           </CardBody>
         </Card>
