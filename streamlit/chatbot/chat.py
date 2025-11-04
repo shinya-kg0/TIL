0e34505063667f5968db21537b36b5bf8e82df33
koga.shinya <shinya4study@gmail.com>
2025-11-04 16:03:19 +0900

    chat page

---
+++ streamlit/chatbot/chat.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from chat_ruby import Rubify
+from chat_eliza import Doctor
+from chat_translation import Translator
+
+def on_select():
+    del st.session_state["transactions"]
+    
+if "ルビ振り" not in st.session_state:
+    st.session_state["ルビ振り"] = Rubify()
+    
+if "セラピー" not in st.session_state:
+    st.session_state["セラピー"] = Doctor()
+    
+if "日英通訳" not in st.session_state:
+    st.session_state["日英通訳"] = Translator()
+    
+if "transactions" not in st.session_state:
+    st.session_state["transactions"] = []
+    
+
+options = ['ルビ振り', 'セラピー', '日英通訳']
+
+with st.sidebar:
+    st.header("チャットサービス")
+    selected = st.selectbox(
+        label="サービスを選んでください",
+        options=options,
+        index=0,
+        on_change=on_select
+    )
+    
+chat = st.session_state[selected]
+st.html(f"> {chat.initial()}")
+
+text = st.chat_input()
+
+if text:
+    response = chat.respond(text)
+    st.session_state.transactions.append(text)
+    st.session_state.transactions.append(f"> {response}")
+    
+for show in st.session_state["transactions"]:
+    st.html(show)
