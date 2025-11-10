62ba470b7fe78c38c127358b01e14a6ad3566145
koga.shinya <shinya4study@gmail.com>
2025-11-10 18:28:07 +0900

    imageproc

---
+++ streamlit/imgproc/navigation/page1.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import streamlit as st
+from save import st_render
+
+st.header(":rainbow[元画像]")
+
+def onchage():
+    img = Image.open(st.session_state._image_upload)
+    img.filename = st.session_state._image_upload.name
+    st.session_state.image_upload = img
+    
+st.file_uploader("画像ファイルをアップロードしてください",
+                key="_image_upload", on_change=onchage)
+
+if "image_upload" in st.session_state:
+    img = st.session_state.image_upload
+    st.image(img)
+    st_render(img)
